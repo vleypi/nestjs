4289cbee172daaf7255fbb320ad7c3345ba4caa3
vleypi <vleypi@gmail.com>
2023-01-04 01:42:58 +0500

    users and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from "@nestjs/core"
+import { DocumentBuilder } from "@nestjs/swagger"
+import { SwaggerModule } from "@nestjs/swagger/dist"
 import { AppModule } from "./app.module"
 
 async function start(){
     const PORT = process.env.PORT || 5000
     const app = await NestFactory.create(AppModule)
+
+    const config = new DocumentBuilder()
+        .setTitle('Nestjs study')
+        .setDescription('Nestjs')
+        .setVersion('1.0.0')
+        .addTag('Vleypi')
+        .build()
+
+    const document = SwaggerModule.createDocument(app,config)
+    SwaggerModule.setup('/api/docs',app,document)
 
     await app.listen(PORT,()=> console.log(`Server Started on port = ${PORT}`))
 }
