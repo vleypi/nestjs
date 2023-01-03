467ee1264b7353bf65bf6483b85a3ec21bfe3df9
vleypi <vleypi@gmail.com>
2023-01-04 00:14:12 +0500

    env

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from "@nestjs/common";
+import {ConfigModule} from '@nestjs/config'
+import { SequelizeModule } from "@nestjs/sequelize";
+import { UsersModule } from './users/users.module';
+
+
+@Module({
+    controllers: [],
+    providers: [],
+    
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: '.env'
+        }),
+        SequelizeModule.forRoot({
+          dialect: 'postgres',
+          host: process.env.POSTGRES_HOST,
+          port: Number(process.env.POSTGRES_PORT),
+          username: process.env.POSTGRES_USER,
+          password: process.env.POSTGRES_PASSWORD,
+          database: process.env.POSTGRES_DB,
+          models: [],
+          autoLoadModels: true
+        }),
+        UsersModule,
+    ],
+})
+
+export class AppModule {
+
+}
